e0982c3017b624016d4e77e84d3002a4cf5dda8d
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2024-03-28 00:49:12 +0700

    Create variabel.js

---
+++ variabel.js
@@ -0,0 +1,13 @@
+const Huruf = "Halo";
+const Angka = 123;
+const pecahan = 1.2;
+const bool1 = true;
+const bool2 = false;
+const arr = [1, 2, 3, 4, 5];
+
+console.log(Huruf);
+console.log(Angka);
+console.log(pecahan);
+console.log(bool1);
+console.log(bool2);
+console.log(arr);
